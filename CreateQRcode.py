7067a27085517c669f4b2901b87eb9a8b2e6e8af
Azii <violetlovato05@gmail.com>
2022-02-17 21:48:32 +0800

    Finally done with all the data, only testing left

--- CreateQRcode.py
+++ CreateQRcode.py
@@ -1,5 +1,15 @@
+from distutils.log import info
+from re import A
+from textwrap import indent
 import qrcode
 
-playlist = qrcode.make('https://open.spotify.com/playlist/3UgqBpRbf8mFxAQ4JtT6lq?si=01b8fb23967d4c76')
+def Info():
+    name = ('Akane Sei')
+    Location = ('Osaka, Japan')
+    Birthdate = ('October 26, 2002')
+    Age= ('19 years old')
+    Date = ('04/05/2022')
+    return (f'Name:{name}    Location: {Location}    Birthdate: {Birthdate}     Age: {Age}     Date today: {Date}     ')
 
-playlist.save('Spotify Recommendations.jpg')
+data =qrcode.make(Info())
+data.save('Profile.jpg')
--- QrCodeRead.py
+++ QrCodeRead.py
@@ -6,8 +6,11 @@ import cv2
 from cv2 import VideoCapture
 import numpy as np
 from pyzbar.pyzbar import decode
+import datetime
 
-#profile = cv2.imread('Github.jpg')
+def print_time():
+    time = datetime.datetime.now() 
+    return time.strftime("%d-%m-%Y %H:%M:%S")
 
 scan = cv2.VideoCapture(0)
 scan.set(3,640)
@@ -17,7 +20,13 @@ while True:
     okinayan, profile = scan.read()
     for qr in decode(profile):
         github = qr.data.decode('utf-8')
-        print(github)
+        side = np.array([qr.polygon],np.int32)
+        side = side.reshape((-1,1,2))
+        cv2.polylines(profile,[side],True,(215,252,100))
+        with open("QrData.txt", "w") as txt:
+            txt.write(github)
+            txt.write(print_time())
 
-    cv2.imshow('Huli',profile)
+
+    cv2.imshow('Huli, akin na data mo',profile)
     cv2.waitKey(1)
